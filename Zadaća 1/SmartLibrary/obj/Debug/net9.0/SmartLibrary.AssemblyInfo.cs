370b4bb8f08d14f5ba05b26a74ad25262eaf2bd8
Kemal Mešić <153745485+mesicc@users.noreply.github.com>
2025-11-21 16:29:27 +0100

    Zadaca 2 - Brisanje

--- Zadaća 1/SmartLibrary/obj/Debug/net9.0/SmartLibrary.AssemblyInfo.cs
+++ Zadaća 1/SmartLibrary/obj/Debug/net9.0/SmartLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+851d092165a523352da8ff4d94181d4c8867def8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc771e66ab7a41d02d40df9d3493d60587d21f27")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
